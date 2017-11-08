2aeda2d262efbe9667222b08e13100e5d3bd8a2c
Tom Ka Chun Cheung <tomhk1123@gmail.com>
2017-11-08 17:09:01 -0500

    async for weather fetch

--- components/Weather.js
+++ components/Weather.js
@@ -1,20 +1,32 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Alert } from 'react-native';
+import axios from 'axios';
+import { OPEN_MAP_API } from 'react-native-dotenv';
+
+const OPEN_WEATHER_URL = `http://api.openweathermap.org/data/2.5/weather?appid=${ OPEN_MAP_API }`;
 import WeatherForm from './WeatherForm';
 import WeatherDetail from './WeatherDetail';
+import { getTemp } from '../api/openWeatherapi';
+
+
 
 class Weather extends Component {
   constructor(props) {
     super(props);
     this.state = { location: 'New York', temp: 88 };
   }
-  getTemperature = (location) => {
-    this.setState({
-      location,
-      temp: 24
-    })
-  };
+  onSearch = (location) => {
+    getTemp(location).then((temp) => {
+      if (temp) {
+        return this.setState({ location, temp });
+      }
+      return console.log('error');
+    }).catch((err) => {
+       console.log('err');
+     }
+    );
 
+  };
   render() {
     const { temp, location } = this.state;
     return (
@@ -22,9 +34,8 @@ class Weather extends Component {
         <Text>
           Find me in Weather
         </Text>
-        <WeatherForm onSearch={ this.getTemperature }/>
+        <WeatherForm onSearch={ this.onSearch }/>
         <WeatherDetail location={this.state.location} temp={this.state.temp}/>
-
       </View>
     );
   }
